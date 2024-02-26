83f5d118c106a352905de3555f32977ea761a46a
zZach77 <96450345+zZach77@users.noreply.github.com>
2024-02-26 02:20:05 -0800

    Passes tests 1, 5, and 6. Doesn't pass 2, 3, and 4
    Creates output file "results.txt" which is where output is

--- Tokenization.cpp
+++ Tokenization.cpp
@@ -35,6 +35,7 @@ enum TokenType {
     PLUS,
     MINUS,
     DIVIDE,
+    ASTERISK,
     MODULO,
     CARAT,
     LT,
@@ -69,10 +70,36 @@ vector<Token> tokenize(const string& input) {
     string currentToken; //Read char by char
     bool inComment = false;
     bool inString = false;
+    int lineNumber = 1;
 
     for (size_t i=0;i < input.size(); ++i) {
+
+        if (inString) {
+            Token stringToken;
+            stringToken.type = STRING;
+
+            while (input[i] != '"') {
+                stringToken.character += input[i++];
+            }
+
+            tokens.push_back(stringToken);
+            
+        }
+
+        //if (std::isspace(input[i]))
+          //  continue;
+
+        char nextChar = i + 1 < input.size() ? input[i + 1] : 0; 
+
         Token inputToken;
         switch(input[i]){
+            case ' ':
+                break;
+
+            case '\n':
+                lineNumber++;
+                break;
+
             case '(':
                 inputToken.type = LEFT_PARENTHESIS;
                 inputToken.character = "(";
@@ -87,7 +114,7 @@ vector<Token> tokenize(const string& input) {
 
             case '[':
                 inputToken.type = LEFT_BRACKET;
-                inputToken.character = "[]";
+                inputToken.character = "[";
                 tokens.push_back(inputToken);
                 break;
 
@@ -98,13 +125,13 @@ vector<Token> tokenize(const string& input) {
                 break;
 
             case '{':
-                inputToken.type = LEFT_BRACKET;
+                inputToken.type = LEFT_BRACE;
                 inputToken.character = "{";
                 tokens.push_back(inputToken);
                 break;
 
             case '}':
-                inputToken.type = RIGHT_BRACKET;
+                inputToken.type = RIGHT_BRACE;
                 inputToken.character = "}";
                 tokens.push_back(inputToken);
                 break;
@@ -113,6 +140,7 @@ vector<Token> tokenize(const string& input) {
                 inputToken.type = DOUBLE_QUOTE;
                 inputToken.character = "\"";
                 tokens.push_back(inputToken);
+                inString = !inString;
                 break;
 
             case '\'':
@@ -134,9 +162,17 @@ vector<Token> tokenize(const string& input) {
                 break;
 
             case '=':
-                inputToken.type = ASSIGNMENT;
-                inputToken.character = "=";
-                tokens.push_back(inputToken);
+                if (nextChar == '=') {
+                    inputToken.type = BOOLEAN_EQUAL;
+                    inputToken.character = "==";
+                    tokens.push_back(inputToken);
+                    i++;
+                } else {
+                    inputToken.type = ASSIGNMENT;
+                    inputToken.character = "=";
+                    tokens.push_back(inputToken);
+                }
+                
                 break;
 
             case '+':
@@ -146,14 +182,33 @@ vector<Token> tokenize(const string& input) {
                 break;
 
             case '-':
-                inputToken.type = MINUS;
-                inputToken.character = "-";
-                tokens.push_back(inputToken);
+                if (std::isdigit(nextChar)) {
+                    i++;
+                    inputToken.type = INTEGER;
+                    inputToken.character = '-';
+                    while (std::isdigit(input[i])) {
+                        inputToken.character += input[i++];
+                    }
+
+                    i--;
+                    tokens.push_back(inputToken);
+                } else {
+                    inputToken.type = MINUS;
+                    inputToken.character = "-";
+                    tokens.push_back(inputToken);
+                }
+            
                 break;
 
             case '\\':
                 inputToken.type = DIVIDE;
                 inputToken.character = "\\";
+                tokens.push_back(inputToken);
+                break;
+
+            case '*':
+                inputToken.type = ASTERISK;
+                inputToken.character = "*";
                 tokens.push_back(inputToken);
                 break;
 
@@ -170,66 +225,243 @@ vector<Token> tokenize(const string& input) {
                 break;
             
             case '<':
-                inputToken.type = LT;
-                inputToken.character = "<";
-                tokens.push_back(inputToken);
+                if (nextChar == '=') {
+                    inputToken.type = LT_EQUAL;
+                    inputToken.character = "<=";
+                    tokens.push_back(inputToken);
+                    i++;
+                } else {
+                    inputToken.type = LT;
+                    inputToken.character = "<";
+                    tokens.push_back(inputToken);
+                }
+                
                 break;
             
             case '>':
-                inputToken.type = GT;
-                inputToken.character = ">";
-                tokens.push_back(inputToken);
-                break;
-            
-            case '<=':
-                inputToken.type = LT_EQUAL;
-                inputToken.character = "<=";
-                tokens.push_back(inputToken);
-                break;
+                if (nextChar == '=') {
+                    inputToken.type = GT_EQUAL;
+                    inputToken.character = ">=";
+                    tokens.push_back(inputToken);
+                    i++;
+                } else {
+                    inputToken.type = GT;
+                    inputToken.character = ">";
+                    tokens.push_back(inputToken);
+                }
 
-            case '>=':
-                inputToken.type = GT_EQUAL;
-                inputToken.character = ">=";
-                tokens.push_back(inputToken);
                 break;
 
             case '&':
-                inputToken.type = BOOLEAN_AND_OPERATOR;
-                inputToken.character = "&";
-                tokens.push_back(inputToken);
+                if (nextChar == '&') {
+                    inputToken.type = BOOLEAN_AND_OPERATOR;
+                    inputToken.character = "&&";
+                    tokens.push_back(inputToken);
+                    i++;
+                }
+                
                 break;
 
             case '|':
-                inputToken.type = BOOLEAN_OR_OPERATOR;
-                inputToken.character = "|";
-                tokens.push_back(inputToken);
+                if (nextChar == '|') {
+                    inputToken.type = BOOLEAN_OR_OPERATOR;
+                    inputToken.character = "||";
+                    tokens.push_back(inputToken);
+                    i++;
+                }
+                
                 break;
             
             case '!':
-                inputToken.type = BOOLEAN_NOT_OPERATOR;
-                inputToken.character = "!";
+                if (nextChar == '=') {
+                    inputToken.type = BOOLEAN_NOT_EQUAL;
+                    inputToken.character = "!=";
+                    tokens.push_back(inputToken);
+                    i++;
+                } else {
+                    inputToken.type = BOOLEAN_NOT_OPERATOR;
+                    inputToken.character = "!";
+                    tokens.push_back(inputToken);
+                }
+
+                break;
+
+            default:
+                if (std::isdigit(input[i])) {
+                    while (!std::isspace(input[i]) && input[i] != ')' && input[i] != ';') {
+                        if (!isdigit(input[i])) {
+                            std::cout << "Syntax error on line " << lineNumber << ": invalid integer\n";
+                            exit(0);
+                        }
+
+                        inputToken.character += input[i++];
+                    }
+
+                    inputToken.type = INTEGER;
+                    
+                } else {
+                    while (!std::isspace(input[i]) && input[i] != ')' && input[i] != ';') {
+                        inputToken.character += input[i++];
+                    }
+
+                    inputToken.type = IDENTIFIER;
+                }
+
+                i--;
                 tokens.push_back(inputToken);
+
                 break;
         }
     }
+
+    return tokens;
 }
 
 
 void displayTokens(const vector<Token>& tokens) {
-    cout << "Token list: " << endl << endl;
+    std::ofstream outputFile("results.txt");
+
+    if (!outputFile) {
+        std::cerr << "Couldn't open output file\n";
+        exit(1);
+    }
+
+    outputFile << "Token list: " << endl << endl;
     for (const auto& token: tokens) {
-        cout << "Token type: ";
+        std::string tokenType = "Token type: ";
+        std::string tokenString = "Token: ";
         switch (token.type) {
+            case IDENTIFIER:
+                tokenType += "IDENTIFIER";
+                break;
+
+            case INTEGER:
+                tokenType += "INTEGER";
+                break;
+
+            case STRING:
+                tokenType += "STRING";
+                break;
 
             case LEFT_PARENTHESIS:
-            cout << "LEFT_PARENTHESIS";
-            break;
+                tokenType += "LEFT_PARENTHESIS";
+                break;
 
+            case RIGHT_PARENTHESIS:
+                tokenType += "RIGHT_PARENTHESIS";
+                break;
 
+            case LEFT_BRACKET:
+                tokenType += "LEFT_BRACKET";
+                break;
 
+            case RIGHT_BRACKET:
+                tokenType += "RIGHT_BRACKET";
+                break;
+
+            case LEFT_BRACE:
+                tokenType += "LEFT_BRACE";
+                break;
+
+            case RIGHT_BRACE:
+                tokenType += "RIGHT_BRACE";
+                break;
+
+            case DOUBLE_QUOTE:
+                tokenType += "DOUBLE_QUOTE";
+                break;
+
+            case SINGLE_QUOTE:
+                tokenType += "SINGLE_QUOTE";
+                break;
+
+            case SEMICOLON:
+                tokenType += "SEMICOLON";
+                break;
+
+            case COMMA:
+                tokenType += "COMMA";
+                break;
+
+            case ASSIGNMENT:
+                tokenType += "ASSIGNMENT";
+                break;
+
+            case PLUS:
+                tokenType += "PLUS";
+                break;
+
+            case MINUS:
+                tokenType += "MINUS";
+                break;
+
+            case DIVIDE:
+                tokenType += "DIVIDE";
+                break;
+
+            case ASTERISK:
+                tokenType += "ASTERISK";
+                break;
+
+            case MODULO:
+                tokenType += "MODULO";
+                break;
+
+            case CARAT:
+                tokenType += "CARAT";
+                break;
+
+            case LT:
+                tokenType += "LT";
+                break;
+
+            case GT:
+                tokenType += "GT";
+                break;
+
+            case LT_EQUAL:
+                tokenType += "LT_EQUAL";
+                break;
+
+            case GT_EQUAL:
+                tokenType += "GT_EQUAL";
+                break;
+        
+            case BOOLEAN_AND_OPERATOR:
+                tokenType += "BOOLEAN_AND_OPERATOR";
+                break;
+
+            case BOOLEAN_OR_OPERATOR:
+                tokenType += "BOOLEAN_OR_OPERATOR";
+                break;
+
+            case BOOLEAN_NOT_OPERATOR:
+                tokenType += "BOOLEAN_NOT_OPERATOR";
+                break;
+
+            case BOOLEAN_EQUAL:
+                tokenType += "BOOLEAN_EQUAL";
+                break;
+
+            case BOOLEAN_NOT_EQUAL:
+                tokenType += "BOOLEAN_NOT_EQUAL";
+                break;
+
+            case BOOLEAN_TRUE:
+                tokenType += "BOOLEAN_TRUE";
+                break;
+
+            case BOOLEAN_FALSE:
+                tokenType += "BOOLEAN_FALSE";
+                break;
 
         }
+
+        tokenString += token.character;
+        outputFile << tokenType << '\n' << tokenString << '\n' << '\n';
     }
+
+    outputFile.close();
 }
 
 
@@ -374,7 +606,8 @@ enum State {
     //     cout << result; 
     // }
     inputFile.close(); // Close the file when we are done working.
-    tokenize(result);
+    vector<Token> tokens = tokenize(result);
+    displayTokens(tokens);
     // return 0;
 }
 
---
+++ idk.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(void) {
+    std::string idk = "i l\nove\ntac\no bell";
+    std::string token;
+
+    for (size_t i=0;i < idk.size(); ++i) {
+        std::getline()
+    }
+
+
+    return 0;
+}
